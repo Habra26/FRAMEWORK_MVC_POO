72f76cea0314244b0ea4e21a3f889a6a7d7666f1
Habra26 <arnaud.habraken@outlook.com>
2025-11-18 13:14:26 +0100

    Traduction des helpers en Helpers

---
+++ core/helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Core;
+
+abstract class Helpers
+{
+    public static function truncate(string $string, int $lg_max = 100): string
+    {
+        if (strlen($string) > $lg_max):
+            $string = substr($string, 0, $lg_max);
+            $last_space = strrpos($string, " ");
+            return substr($string, 0, $last_space) . "...";
+        endif;
+        return $string;
+    }
+}
