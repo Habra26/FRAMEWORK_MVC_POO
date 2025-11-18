1393bc7234e0512fd3c72189605b57265056c754
Habra26 <arnaud.habraken@outlook.com>
2025-11-18 10:06:37 +0100

    Route pages.home
    Sans mise en page et sans authors

---
+++ app/controllers/pagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\pagesController;
+use \PDO;
+
+function homeAction(PDO $conn) {
+    include_once '../app/models/booksModel.php';
+    $books = \App\Models\BooksModel\findAll($conn, 3);
+
+    global $content, $title;
+    ob_start();
+    include '../app/views/pages/home.php';
+    $content = ob_get_clean();
+}
--- app/routers/index.php
+++ app/routers/index.php
@@ -4,3 +4,6 @@
 // PATERN: /
 // CTRL: pagesController
 // ACTION: homeAction
+
+include_once '../app/controllers/pagesController.php';
+\App\Controllers\PagesController\homeAction($conn);
