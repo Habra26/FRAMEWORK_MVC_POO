8efa67105cec58581154b24d0aef2eacaa0c53cf
Habra26 <arnaud.habraken@outlook.com>
2025-11-25 09:55:58 +0100

    Décomposition des _recents et _index de books et authors

---
+++ app/views/authors/_index.php
@@ -0,0 +1,7 @@
+<ul>
+    <?php foreach ($authors as $author): ?>
+        <li>
+            <?php echo $author->firstname; ?>     <?php echo $author->lastname; ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
